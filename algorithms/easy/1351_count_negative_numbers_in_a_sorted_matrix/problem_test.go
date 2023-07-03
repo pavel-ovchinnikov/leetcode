f62f712a5ffa39e63b5ec321110c603a9c03674a
Pavel Ovchinnikov <povchinnikov.life@icloud.com>
2023-07-03 15:56:27 +0300

    1351

---
+++ algorithms/easy/1351_count_negative_numbers_in_a_sorted_matrix/problem_test.go
@@ -0,0 +1,23 @@
+package problem
+
+import (
+	"testing"
+)
+
+type testCase struct {
+	grid     [][]int
+	expected int
+}
+
+var tests = []testCase{
+	{[][]int{{4, 3, 2, -1}, {3, 2, 1, -1}, {1, 1, -1, -2}, {-1, -1, -2, -3}}, 8},
+	{[][]int{{3, 2}, {1, 0}}, 0},
+}
+
+func Test(t *testing.T) {
+	for _, test := range tests {
+		if output := countNegatives(test.grid); output != test.expected {
+			t.Errorf("Output %q not equal to expected %q", output, test.expected)
+		}
+	}
+}
