9de244069ea2045df3571ea7ddb93e2659300338
Pavel Ovchinnikov <povchinnikov.life@icloud.com>
2023-07-06 13:14:04 +0300

    268. Missing Number

---
+++ algorithms/easy/0268.Missing-Number/268. Missing Number.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func missingNumber(nums []int) int {
+	n := len(nums)
+	sum := (n * (n + 1)) / 2
+	for _, v := range nums {
+		sum -= v
+	}
+	return sum
+}
+
+func main() {
+	fmt.Println(missingNumber([]int{3, 0, 1}))
+}
