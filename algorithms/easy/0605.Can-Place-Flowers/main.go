020963ddfc138a6115c50fc03d5107f12d444ccb
Pavel Ovchinnikov <povchinnikov.life@icloud.com>
2023-07-15 12:09:01 +0300

    0605.Can-Place-Flowers

---
+++ algorithms/easy/0605.Can-Place-Flowers/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func canPlaceFlowers(flowerbed []int, n int) bool {
+	for i, flower := range flowerbed {
+		if flower == 1 {
+			continue
+		}
+		if (i == 0 || flowerbed[i-1] == 0) && (i == len(flowerbed)-1 || flowerbed[i+1] == 0) {
+			flowerbed[i] = 1
+			n--
+		}
+	}
+	return n <= 0
+}
+
+func main() {
+	fmt.Println(canPlaceFlowers([]int{1, 0, 0, 0, 1}, 1))
+	fmt.Println(canPlaceFlowers([]int{1, 0, 0, 0, 1}, 2))
+}
