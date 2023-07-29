203540a01258596514a1e30a3ffbca83faf6a827
Pavel Ovchinnikov <povchinnikov.life@icloud.com>
2023-07-29 19:52:39 +0300

    26. Remove Duplicates from Sorted Array

---
+++ algorithms/easy/0026.Remove-Duplicates-from-Sorted-Array/26. Remove Duplicates from Sorted Array.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func removeDuplicates(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+
+	j := 0
+	for i := 1; i < len(nums); i++ {
+		if nums[i] != nums[j] {
+			j++
+			nums[j] = nums[i]
+		}
+	}
+	return j + 1
+}
+
+func main() {
+	fmt.Println(removeDuplicates([]int{1, 1, 2}))
+}
