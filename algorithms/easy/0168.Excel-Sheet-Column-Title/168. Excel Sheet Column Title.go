f90eb49b35ab774d33aa9568d0152a4b11c8d5ed
Pavel Ovchinnikov <povchinnikov.life@icloud.com>
2023-08-22 08:04:37 +0300

    168. Excel Sheet Column Title

--- algorithms/easy/0168.Excel-Sheet-Column-Title/168. Excel Sheet Column Title.go
+++ algorithms/easy/0168.Excel-Sheet-Column-Title/168. Excel Sheet Column Title.go
@@ -3,15 +3,25 @@ package main
 import "fmt"
 
 func convertToTitle(columnNumber int) string {
-	var res string
+	res := make([]rune, 0, columnNumber/26)
 
 	for columnNumber != 0 {
 		index := (columnNumber - 1) % 26
 		columnNumber = (columnNumber - 1) / 26
-		res = string(rune(index)+65) + res
+		res = append(res, rune(index)+65)
 	}
 
-	return res
+	reverse(res)
+	return string(res)
+}
+
+func reverse(word []rune) {
+	l, r := 0, len(word)-1
+	for l < r {
+		word[l], word[r] = word[r], word[l]
+		l++
+		r--
+	}
 }
 
 func main() {
