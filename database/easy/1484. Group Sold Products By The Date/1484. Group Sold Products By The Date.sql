2a8d149d43907b6f107af331a9e962de055bbaa1
Pavel Ovchinnikov <povchinnikov.life@icloud.com>
2023-08-25 20:37:29 +0300

    1484. Group Sold Products By The Date

---
+++ database/easy/1484. Group Sold Products By The Date/1484. Group Sold Products By The Date.sql
@@ -0,0 +1,7 @@
+SELECT  
+	sell_date,
+	COUNT(sell_date) AS num_sold,
+	GROUP_CONCAT(distinct product  ORDER BY product) AS products 
+FROM 
+	(SELECT DISTINCT sell_date,product FROM Activities) AS Activities
+GROUP BY sell_date;
