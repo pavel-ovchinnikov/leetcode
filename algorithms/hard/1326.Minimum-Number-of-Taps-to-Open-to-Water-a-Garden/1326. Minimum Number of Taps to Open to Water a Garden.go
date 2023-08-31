67ccd395230a03fc5358fe4b9484c600e6dde2c8
Pavel Ovchinnikov <povchinnikov.life@icloud.com>
2023-08-31 13:46:33 +0300

    1326. Minimum Number of Taps to Open to Water a Garden

--- algorithms/hard/1326.Minimum-Number-of-Taps-to-Open-to-Water-a-Garden/1326. Minimum Number of Taps to Open to Water a Garden.go
+++ algorithms/hard/1326.Minimum-Number-of-Taps-to-Open-to-Water-a-Garden/1326. Minimum Number of Taps to Open to Water a Garden.go
@@ -1,30 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func minTaps(n int, ranges []int) int {
-	arr := make([]int, n+1)
-	for i, rng := range ranges {
-		l := max(0, i-rng)
-		r := min(n, i+rng)
-		arr[l] = max(arr[l], r)
+	arr := make([][]int, n+1)
+	for i := range arr {
+		arr[i] = make([]int, 2)
+		arr[i][0] = max(0, i-ranges[i])
+		arr[i][1] = min(n, i+ranges[i])
 	}
 
-	globalMax := arr[0]
-	localMax := arr[0]
-	count := 1
-	for i := 0; i < n; i++ {
-		if i > globalMax {
+	sort.Slice(arr, func(i, j int) bool {
+		if arr[i][0] == arr[j][0] {
+			return arr[j][1] < arr[i][1]
+		}
+		return arr[i][0] < arr[j][0]
+	})
+
+	if arr[0][0] > 0 {
+		return -1
+	}
+	end := arr[0][1]
+	cnt := 0
+	for i := range arr {
+		cnt++
+		if end >= n {
+			return cnt
+		}
+		nextEnd := end
+		for j := i + 1; j < len(arr); j++ {
+			if arr[j][0] <= end && arr[j][1] > nextEnd {
+				i = j
+				nextEnd = arr[j][1]
+			}
+		}
+		if nextEnd == end {
 			return -1
 		}
-
-		globalMax = max(globalMax, arr[i])
-		if i == localMax {
-			localMax = globalMax
-			count++
-		}
+		end = nextEnd
+		i--
 	}
-	return count
+	return -1
 }
 
 func min(a, b int) int {
