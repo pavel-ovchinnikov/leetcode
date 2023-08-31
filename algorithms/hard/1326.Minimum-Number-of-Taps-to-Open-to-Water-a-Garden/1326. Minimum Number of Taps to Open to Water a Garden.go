0f6a10846402eb8abdeac269221087de90333d35
Pavel Ovchinnikov <povchinnikov.life@icloud.com>
2023-08-31 13:40:08 +0300

    1326. Minimum Number of Taps to Open to Water a Garden

--- algorithms/hard/1326.Minimum-Number-of-Taps-to-Open-to-Water-a-Garden/1326. Minimum Number of Taps to Open to Water a Garden.go
+++ algorithms/hard/1326.Minimum-Number-of-Taps-to-Open-to-Water-a-Garden/1326. Minimum Number of Taps to Open to Water a Garden.go
@@ -6,7 +6,7 @@ func minTaps(n int, ranges []int) int {
 	arr := make([]int, n+1)
 	for i, rng := range ranges {
 		l := max(0, i-rng)
-		r := i + rng
+		r := min(n, i+rng)
 		arr[l] = max(arr[l], r)
 	}
 
@@ -27,6 +27,13 @@ func minTaps(n int, ranges []int) int {
 	return count
 }
 
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
 func max(a, b int) int {
 	if a < b {
 		return b
@@ -39,5 +46,6 @@ func main() {
 	fmt.Println(minTaps(3, []int{0, 0, 0, 0}))
 	fmt.Println(minTaps(5, []int{1, 1, 1, 1, 1, 1}))
 	fmt.Println(minTaps(10, []int{1, 4, 1, 1, 0, 0, 2, 1, 5, 6, 0}))
+	fmt.Println(minTaps(5, []int{3, 0, 1, 1, 0, 0}))
 
 }
