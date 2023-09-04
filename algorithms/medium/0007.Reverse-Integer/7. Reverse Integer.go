93404de5ba531b99e4b620160dc5ba7d464efbe4
Pavel Ovchinnikov <povchinnikov.life@icloud.com>
2023-09-04 09:21:53 +0300

    7. Reverse Integer

---
+++ algorithms/medium/0007.Reverse-Integer/7. Reverse Integer.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func reverse(x int) int {
+	res := 0
+	for x != 0 {
+		res = res*10 + x%10
+		x = x / 10
+
+		if res < math.MinInt32 || res > math.MaxInt32 {
+			return 0
+		}
+	}
+
+	return res
+}
+
+func main() {
+	fmt.Println(reverse(123456))
+}
