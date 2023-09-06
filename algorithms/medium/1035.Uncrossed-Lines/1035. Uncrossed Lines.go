86be2af5b25ff60fe860dd2374b35c1c97dd57de
Pavel Ovchinnikov <povchinnikov.life@icloud.com>
2023-09-06 16:04:25 +0300

    1035. Uncrossed Lines

---
+++ algorithms/medium/1035.Uncrossed-Lines/1035. Uncrossed Lines.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func maxUncrossedLines(nums1 []int, nums2 []int) int {
+	n, m := len(nums1), len(nums2)
+	dp := make([][]int, n+1)
+	for i := range dp {
+		dp[i] = make([]int, m+1)
+	}
+
+	for i := 1; i <= n; i++ {
+		for j := 1; j <= m; j++ {
+			if nums1[i-1] == nums2[j-1] {
+				dp[i][j] = 1 + dp[i-1][j-1]
+			} else {
+				dp[i][j] = max(dp[i][j-1], dp[i-1][j])
+			}
+		}
+	}
+	for i := range dp {
+		fmt.Println(dp[i])
+	}
+
+	return dp[n][m]
+}
+
+func max(a, b int) int {
+	if a < b {
+		return b
+	}
+	return a
+}
+
+func main() {
+	fmt.Println(maxUncrossedLines([]int{2, 5, 1, 2, 5}, []int{10, 5, 2, 1, 5, 2}))
+}
