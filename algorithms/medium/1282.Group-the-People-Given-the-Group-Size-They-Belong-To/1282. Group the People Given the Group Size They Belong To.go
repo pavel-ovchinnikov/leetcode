395c816c5dfd771148ffffc7fd72c0d10f72cfa6
Pavel Ovchinnikov <povchinnikov.life@icloud.com>
2023-09-11 21:05:49 +0300

    1282. Group the People Given the Group Size They Belong To

--- algorithms/medium/1282.Group-the-People-Given-the-Group-Size-They-Belong-To/1282. Group the People Given the Group Size They Belong To.go
+++ algorithms/medium/1282.Group-the-People-Given-the-Group-Size-They-Belong-To/1282. Group the People Given the Group Size They Belong To.go
@@ -2,19 +2,36 @@ package main
 
 import "fmt"
 
-func groupThePeople(groupSizes []int) [][]int {
-	hashmap := make(map[int][]int)
+// func groupThePeople(groupSizes []int) [][]int {
+// 	hashmap := make(map[int][]int)
 
-	for index, size := range groupSizes {
-		hashmap[size] = append(hashmap[size], index)
+// 	for index, size := range groupSizes {
+// 		hashmap[size] = append(hashmap[size], index)
+// 	}
+
+// 	res := [][]int{}
+// 	for size, people := range hashmap {
+// 		rem := len(people)
+// 		for rem > 0 {
+// 			res = append(res, people[rem-size:rem])
+// 			rem -= size
+// 		}
+// 	}
+
+// 	return res
+// }
+
+func groupThePeople(groupSizes []int) [][]int {
+	hash := map[int][]int{}
+
+	for i := 0; i < len(groupSizes); i++ {
+		hash[groupSizes[i]] = append(hash[groupSizes[i]], i)
 	}
 
-	res := [][]int{}
-	for size, people := range hashmap {
-		rem := len(people)
-		for rem > 0 {
-			res = append(res, people[rem-size:rem])
-			rem -= size
+	res := make([][]int, 0, len(hash))
+	for size, nums := range hash {
+		for i := 0; i < len(nums); i += size {
+			res = append(res, nums[i:i+size])
 		}
 	}
 
