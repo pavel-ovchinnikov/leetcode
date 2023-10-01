224bc6fb652996e447e260e6ae8e61af4ee278fd
Pavel Ovchinnikov <povchinnikov.life@icloud.com>
2023-10-01 20:36:36 +0300

    680. Valid Palindrome II

---
+++ algorithms/easy/0680.Valid-PalindromeII/680. Valid Palindrome II.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func validPalindrome(s string) bool {
+	n := len(s)
+	l, r := 0, n-1
+	for l < r {
+		if s[l] != s[r] {
+			return isPalindrome(s, l+1, r) || isPalindrome(s, l, r-1)
+		}
+		l++
+		r--
+	}
+	return true
+}
+
+func isPalindrome(s string, l, r int) bool {
+	for l < r {
+		if s[l] != s[r] {
+			return false
+		}
+		l++
+		r--
+	}
+	return true
+}
+
+func main() {
+	fmt.Println(validPalindrome("abca"))
+	fmt.Println(validPalindrome("abxcddcba"))
+}
