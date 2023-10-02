72e368f9bf93c229533ff5f57967b64027f8801e
Pavel Ovchinnikov <povchinnikov.life@icloud.com>
2023-10-02 10:42:28 +0300

    697.Degree-of-an-Array

---
+++ algorithms/easy/0697.Degree-of-an-Array/697.Degree-of-an-Array.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func findShortestSubArray(nums []int) int {
+	res := 1
+	starts := make(map[int]int)
+	counters, maxCounter := make(map[int]int), 1
+
+	for i, num := range nums {
+		if _, ok := starts[num]; ok {
+			counters[num]++
+			if counters[num] > maxCounter {
+				maxCounter = counters[num]
+				res = i - starts[num] + 1
+			} else if counters[num] == maxCounter && i-starts[num] < res {
+				res = i - starts[num] + 1
+			}
+		} else {
+			starts[num], counters[num] = i, 1
+		}
+	}
+	return res
+}
+
+func main() {
+	fmt.Println(findShortestSubArray([]int{1, 2, 2, 3, 1, 4, 2}))
+}
