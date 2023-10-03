fe3bf677f44ded6d720b899799233e7cf1dd7930
Pavel Ovchinnikov <povchinnikov.life@icloud.com>
2023-10-03 20:14:11 +0300

    551. Student Attendance Record I

---
+++ algorithms/easy/0551.Student-Attendance-RecordI/551. Student Attendance Record I.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func checkRecord(s string) bool {
+	absent := 0
+	late := 0
+
+	for i := 0; i < len(s); i++ {
+		if s[i] == 'A' {
+			absent++
+			if absent == 2 {
+				return false
+			}
+		}
+
+		if s[i] == 'L' {
+			late++
+			if late == 3 {
+				return false
+			}
+		} else {
+			late = 0
+		}
+	}
+
+	return true
+}
+
+func main() {
+	fmt.Println(checkRecord("PPALLP"))
+}
