9053ef2c34de820c3b95e03ae9dc03913a402294
Pavel Ovchinnikov <povchinnikov.life@icloud.com>
2023-10-09 20:10:30 +0300

    141. Linked List Cycle

--- algorithms/easy/0141.Linked-List-Cycle/141. Linked List Cycle.go
+++ algorithms/easy/0141.Linked-List-Cycle/141. Linked List Cycle.go
@@ -8,21 +8,35 @@ import (
 
 type ListNode = structures.ListNode
 
+// func hasCycle(head *ListNode) bool {
+// 	if head == nil {
+// 		return false
+// 	}
+// 	cur1 := head
+// 	cur2 := head.Next
+
+// 	for cur1 != nil && cur2 != nil && cur2.Next != nil {
+// 		if cur1 == cur2 {
+// 			return true
+// 		}
+// 		cur1 = cur1.Next
+// 		cur2 = cur2.Next.Next
+// 	}
+// 	return false
+// }
+
 func hasCycle(head *ListNode) bool {
 	if head == nil {
 		return false
 	}
-	cur1 := head
-	cur2 := head.Next
+	l1, l2 := head, head.Next
 
-	for cur1 != nil && cur2 != nil && cur2.Next != nil {
-		if cur1 == cur2 {
-			return true
-		}
-		cur1 = cur1.Next
-		cur2 = cur2.Next.Next
+	for l2 != nil && l2.Next != nil && l1.Next.Next != nil && l1 != l2 {
+		l1 = l1.Next
+		l2 = l2.Next.Next
 	}
-	return false
+
+	return l1 == l2
 }
 
 func main() {
