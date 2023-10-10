00ceb05460a79c3a9ee94b30950066a5377bb3a6
Pavel Ovchinnikov <povchinnikov.life@icloud.com>
2023-10-10 20:00:49 +0300

    2894. Divisible and Non-divisible Sums Difference

--- algorithms/easy/2894.Divisible-and-Non-divisible-Sums-Difference/2894. Divisible and Non-divisible Sums Difference.go
+++ algorithms/easy/2894.Divisible-and-Non-divisible-Sums-Difference/2894. Divisible and Non-divisible Sums Difference.go
@@ -2,15 +2,21 @@ package main
 
 import "fmt"
 
+// func differenceOfSums(n int, m int) int {
+// 	sum1, sum2 := 0, 0
+// 	for i := 1; i <= n; i++ {
+// 		if i%m != 0 {
+// 			sum1 += i
+// 		} else {
+// 			sum2 += i
+// 		}
+// 	}
+// 	return sum1 - sum2
+// }
+
 func differenceOfSums(n int, m int) int {
-	sum1, sum2 := 0, 0
-	for i := 1; i <= n; i++ {
-		if i%m != 0 {
-			sum1 += i
-		} else {
-			sum2 += i
-		}
-	}
+	sum1 := n * (n + 1) / 2
+	sum2 := 2 * m * (n / m) * (n/m + 1) / 2
 	return sum1 - sum2
 }
 
