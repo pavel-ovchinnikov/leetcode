49463e8c2eaf5ac6026a4eff90edaa8b0ea806e8
Pavel Ovchinnikov <povchinnikov.life@icloud.com>
2025-01-02 21:21:28 +0300

    203. Remove Linked List Elements

---
+++ problems/0203-remove-linked-list-elements/solution.go
@@ -0,0 +1,28 @@
+package removelinkedlistelements
+
+import (
+	. "github.com/pavel-ovchinnikov/leetcode/tools/structures/linked_list"
+)
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func removeElements(head *ListNode, val int) *ListNode {
+	newHead := &ListNode{}
+	cur1, cur2 := newHead, head
+	for cur2 != nil {
+		if cur2.Val == val {
+			cur2 = cur2.Next
+			continue
+		}
+
+		cur1.Next = cur2
+		cur1, cur2 = cur1.Next, cur2.Next
+	}
+	cur1.Next = nil
+	return newHead.Next
+}
