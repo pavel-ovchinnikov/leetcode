8f0b0b692f0d7014381915127dbcd77258905686
Pavel Ovchinnikov <povchinnikov.life@icloud.com>
2025-11-04 22:25:06 +0300

    2469. Convert the Temperature

---
+++ tools/slices/compare.go
@@ -0,0 +1,15 @@
+package slices
+
+import "math"
+
+func CompareFloatSlices(a, b []float64, epsilon float64) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i := range a {
+		if math.Abs(a[i]-b[i]) > epsilon {
+			return false
+		}
+	}
+	return true
+}
